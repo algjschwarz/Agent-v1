037d77a6a2b45c6b22a4fb7a58dcf23e13c0d8d7
algjschwarz <149257078+alstryka@users.noreply.github.com>
2026-08-11 20:50:58 -0500

    added function for inputing text to conversation

--- main.py
+++ main.py
@@ -4,29 +4,35 @@ from ollama import ChatResponse
 messages = [
     {
             'role': 'system',
-            'content': 'You are five years old and are a fish'
-    },
-    {
-        'role': 'user',
-        'content': 'how to play fun games?'
+            'content': 'You are a helpful assistant that has access to google'
     }
     ]
 
-stream : ChatResponse = chat(model='gemma4:e4b', messages=messages, think=True, stream=True)
-in_thinking = False
-new_message = {'thinking':'','content':''}
-for chunk in stream:
-    if chunk.message.thinking:
-        if not in_thinking:
-            print("=== THINKING ===")
-            in_thinking = True
-        new_message['thinking'] = new_message['thinking'] + chunk.message.thinking
-        print(chunk.message.thinking, end='', flush=True)
-    if chunk.message.content:
-        if in_thinking:
-            print("\n\n=== ANSWER ===")
-            in_thinking = False
-        new_message['content'] = new_message['content'] + chunk.message.content
-        print(chunk.message.content, end='', flush=True)
-messages.append({'role': 'assistant', 'content': new_message['content'].strip()})
+def stream_response(stream) -> list:
+    is_thinking = False
+    new_message = {'thinking':'','content':''}
+    for chunk in stream:
+        if chunk.message.thinking:
+            if not is_thinking:
+                print("=== THINKING ===")
+                is_thinking = True
+            new_message['thinking'] = new_message['thinking'] + chunk.message.thinking
+            print(chunk.message.thinking, end='', flush=True)
+        if chunk.message.content:
+            if is_thinking:
+                print("\n\n=== ANSWER ===")
+                is_thinking = False
+            new_message['content'] = new_message['content'] + chunk.message.content
+            print(chunk.message.content, end='', flush=True)
+    return new_message
+
+def new_input(text) -> None:
+    messages.append({'role': 'user', 'content': text})
+    stream : ChatResponse = chat(model='gemma4:e4b', messages=messages, think=True, stream=True)
+    new_message = stream_response(stream)
+    messages.append({'role': 'assistant', 'content': new_message['content'].strip()})
+
+while True:
+    new_input(input())
+
 
